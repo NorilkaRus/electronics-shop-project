7acb20630fc261b809b9b940327eb359adfa7b37
Evgeny <norilkarus@gmail.com>
2023-09-19 19:16:43 +0300

    Добавлены @property и @number_of_sim.setter

--- src/phone.py
+++ src/phone.py
@@ -4,14 +4,22 @@ class Phone(Item):
     """
     Класс телефона, наследуемый из класса товара
     """
-    def __init__(self, name: str, price: float, quantity: int, number_of_sim):
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
 
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.__number_of_sim})"
 
 
-phone1 = Phone("iPhone 14", 120_000, 5, 2)
-print(repr(phone1))
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, number):
+        if number > 0:
+            self.__number_of_sim = number
+        else:
+            print("Количество физических SIM-карт должно быть целым числом больше нуля")
